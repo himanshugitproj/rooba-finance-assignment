206b4984da57d047833d270508f7b77cfe15f591
himanshugitproj <himanshuyadav.18gcebit044@galgotiacollege.edu>
2024-05-20 14:52:50 +0530

    Update Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,10 +5,10 @@ const Background = () => {
     <div className='' style={{display:'inline-flex', float:'left', width:'50%'}}>
         <div style={{display:'inline-flex', float:'left', padding:"0px", }}>
             <div style={{display:'inline'}}>
-                <img src="../public/images/Astrix Branding.svg"  />
+                <img src="/images/Astrix Branding.svg"  />
             </div>
             <div style={{display:'flex', paddingLeft:'15px'}}>
-                <img src="../public/images/Astrix.svg"/>
+                <img src="/images/Astrix.svg"/>
             </div>
         </div>
 
