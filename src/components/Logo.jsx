3bbc1ed670ddb88b160e9dd4e9e696e1f9d663e1
himanshugitproj <himanshuyadav.18gcebit044@galgotiacollege.edu>
2024-05-20 14:49:44 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div style={{height:'', width:'90%'}}>
-        <img className='pt-16' src='../public/images/ASTRIXCOLLECTIBLE.png'></img>
+        <img className='pt-16' src='/images/ASTRIXCOLLECTIBLE.png'></img>
     </div>
   )
 }
